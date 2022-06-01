c77601d128b0131b18853f76636c386ed03ba83d
German <Makeev_German@mail.ru@example.com>
2022-06-01 13:52:27 +0300

    task max3

---
+++ max3/Program.cs
@@ -0,0 +1,26 @@
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1,10);
+Console.WriteLine(numberC);
+int maxNumbers = numberA;
+if(maxNumbers>numberB)
+{
+    Console.WriteLine(maxNumbers);
+}
+else
+{
+    maxNumbers=numberB;
+}
+if(maxNumbers>numberC)
+{
+    Console.WriteLine(maxNumbers);
+}
+else
+{
+    maxNumbers=numberC;
+}
+
+Console.WriteLine(maxNumbers);
+
