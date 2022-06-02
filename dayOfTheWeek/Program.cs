405f9691b41dfae84f7fcdd9f63e5b135b2fcef1
German <Makeev_German@mail.ru@example.com>
2022-06-02 13:37:09 +0300

    day of the week

--- dayOfTheWeek/Program.cs
+++ dayOfTheWeek/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Сonsole.WriteLine("введите число дня недели");
+string number = Console.ReadLine();
+if(1=Сonsole.WriteLine("понедельник"));
+else;
+if(2=Сonsole.WriteLine("вторник"));
+else;
+if(3=Сonsole.WriteLine("среда"));
+else;
+if(4=Сonsole.WriteLine("четверг"));
+else;
+if(5=Сonsole.WriteLine("пятница"));
+else;
+if(6=Сonsole.WriteLine("суббота"));
+else;
+if(7=Сonsole.WriteLine("воскресенье"));
+
+Console.Write(number);
