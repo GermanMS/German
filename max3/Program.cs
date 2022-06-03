71c9851bc2fce82f93430e5162c4bdc65e598aca
German <Makeev_German@mail.ru@example.com>
2022-06-03 21:07:58 +0300

    max3

--- max3/Program.cs
+++ max3/Program.cs
@@ -1,22 +1,18 @@
 ﻿int numberA = new Random().Next(1,20);
-Console.WriteLine(numberA);
+Console.WriteLine(numberA + "первое случайное число");
 int numberB = new Random().Next(1,20);
-Console.WriteLine(numberB);
+Console.WriteLine(numberB + "второе случайное число");
 int numberC = new Random().Next(1,20);
-Console.WriteLine(numberC);
+Console.WriteLine(numberC +  "третье случайное число");
 int maxNumbers = numberA;
-if(maxNumbers>numberB)
-{
-    Console.WriteLine(maxNumbers);
-}
+if(maxNumbers>numberB);
+
 else
 {
     maxNumbers=numberB;
 }
-if(maxNumbers>numberC)
-{
-    Console.WriteLine(maxNumbers);
-}
+if(maxNumbers>numberC);
+
 else
 {
     maxNumbers=numberC;
