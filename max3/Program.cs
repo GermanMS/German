389d538299d984ba356f548de6d2bb414f833c65
German <Makeev_German@mail.ru@example.com>
2022-06-18 19:40:54 +0300

    nmax3

--- max3/Program.cs
+++ max3/Program.cs
@@ -5,15 +5,7 @@ Console.WriteLine(numberB + "второе случайное число");
 int numberC = new Random().Next(1,20);
 Console.WriteLine(numberC +  "третье случайное число");
 int maxNumbers = numberA;
-if(maxNumbers>numberB);
-else
-{
-    maxNumbers=numberB;
-}
-if(maxNumbers>numberC);
+if(maxNumbers<numberB) maxNumbers=numberB;
+if(maxNumbers<numberC) maxNumbers=numberC;
 
-else
-{
-    maxNumbers=numberC;
-}
 Console.WriteLine(maxNumbers + " максимальное значение из 3");
