040f00d5999e84237ccc1cb6b9965998f7df7256
German <Makeev_German@mail.ru@example.com>
2022-06-18 18:24:31 +0300

    2numbersoutof3

---
+++ 2numbersoutof3/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine($"{number} Случайное число");
+int numberB = number / 100;
+int numberC = number / 10 % 10;
+int numberD = number % 10;
+int numberA = numberB * 10 + numberD;
+Console.WriteLine($"{numberA}");
