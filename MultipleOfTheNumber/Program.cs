3d4146b28d7aa8206189ab14cb3cc81f171bb1b3
German <Makeev_German@mail.ru@example.com>
2022-06-18 20:29:25 +0300

    MultipleOfTheNumber

---
+++ MultipleOfTheNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = numberA % numberB;
+if (numberC ==0 ){
+    Console.WriteLine(" Первое число кратно второму ");
+}
+else{
+    Console.WriteLine($"{numberC} остаток от деления");
+}
