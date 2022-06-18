5e4651b0ac5805e396e224f0f6d7d72c5a49bb75
German <Makeev_German@mail.ru@example.com>
2022-06-18 16:50:40 +0300

    segment1099

---
+++ segment1099/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(10,100);
+Console.WriteLine($"{number} Случайное число");
+int numberA= number/10;
+int numberB= number%10;
+if( numberA>numberB){
+    Console.WriteLine($"цифра {numberA} больше");
+}
+else{
+ Console.WriteLine($"цифра {numberB} больше");
+
+}
